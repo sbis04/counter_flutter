52a50f44dd1a5881791effecec4dfd0fde4f3c51
Souvik Biswas <sbis1999@gmail.com>
2020-02-09 06:30:53 +0530

    iOS changes and code signing

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/souvikbiswas/Documents/Flutter/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/souvikbiswas/Desktop/Flutter_Resource/Medium Article Apps/counter_app"
-export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_ROOT=/Users/souvikbiswas/development/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/souvikbiswas/Mobile Apps/Flutter/counter_flutter"
+export "FLUTTER_TARGET=/Users/souvikbiswas/Mobile Apps/Flutter/counter_flutter/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
-export "FLUTTER_FRAMEWORK_DIR=/Users/souvikbiswas/Documents/Flutter/flutter/bin/cache/artifacts/engine/ios"
-export "FLUTTER_BUILD_NAME=1.0.0"
-export "FLUTTER_BUILD_NUMBER=1"
+export "FLUTTER_FRAMEWORK_DIR=/Users/souvikbiswas/development/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.0.1"
+export "FLUTTER_BUILD_NUMBER=2"
+export "TRACK_WIDGET_CREATION=true"
